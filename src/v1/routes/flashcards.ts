6e543671838e1f619506a3c082dcdee84b4de2b9
Mkparu Somtochi <mkparusomtochi26@gmail.com>
2023-12-11 10:55:30 +0100

    fix(middlewares): resolve ts error in auth middleware

--- src/v1/routes/flashcards.ts
+++ src/v1/routes/flashcards.ts
@@ -5,8 +5,10 @@ import {
   updateFlashcard,
   updateFlashcardRating
 } from '../controllers/flashcards'
+import auth from '../middlewares/authMiddleware'
 const router = Router()
 
+router.use(auth)
 router.post('/manual-create', createFlashcard)
 router.patch('/:flashcardId', updateFlashcard)
 router.patch('/:flashcardId/rating', updateFlashcardRating)
